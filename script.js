654d054085c2056b72ba809ab1a704d46f7b3521
Mohammad Ishan Ahmad <mdishanahmad@gmail.com>
2026-03-30 17:02:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+let tableEl=document.querySelector("#sampleTable");
 function insert_Row() {
-    //Write your code here
-  
-  
+
+let newrowEl=document.createElement("tr");
+	let newCell1=document.createElement("td");
+  newCell1.textContent="New Cell1"
+	let newCell2=document.createElement("td");
+   newCell2.textContent="New Cell2"
+	newrowEl.appendChild(newCell1);
+	newrowEl.appendChild(newCell2);
+	tableEl.prepend(newrowEl)
 }
+
